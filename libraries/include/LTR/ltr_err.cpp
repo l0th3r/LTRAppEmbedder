11076c35f637c84f8d20587aef112db6dbe28882
Cosmo <58365407+l0th3r@users.noreply.github.com>
2022-06-01 09:16:36 +0100

    Update ltr_err.cpp

--- libraries/include/LTR/ltr_err.cpp
+++ libraries/include/LTR/ltr_err.cpp
@@ -4,10 +4,59 @@ using namespace ltr;
 
 const std::string ltr::ErrContextToStr(const ErrContext context)
 {
-	return // create str
+	switch (context)
+	{
+	default:
+	case ErrContext::App:
+		return "App";
+		break;
+
+	case ErrContext::GLFW:
+		return "GLFW dependency";
+		break;
+
+	case ErrContext::GLEW:
+		return "GLEW dependency";
+		break;
+
+	case ErrContext::Shader:
+		return "Shaders";
+		break;
+	}
 }
 
 const std::string ltr::ErrCodeToStr(const ErrCode code)
 {
-	return "Code " + std::to_string((int)code);
+	switch (code)
+	{
+	case ErrCode::no_err:
+		return "nothing";
+		break;
+
+	case ErrCode::init:
+		return "initialisation";
+		break;
+
+	case ErrCode::terminate:
+		return "terminate";
+		break;
+
+		// > 100 (GLFW)
+	case ErrCode::creating_window:
+		return "creating window";
+		break;
+
+		// > 300 (Shader)
+	case ErrCode::shader_parsing:
+		return "parsing";
+		break;
+
+	case ErrCode::shader_compilation:
+		return "compilation";
+		break;
+
+		// default return raw code
+	default:
+		return "Code " + std::to_string((int)code);
+	}
 }
